f151d8cc61296d735cdb72357a384e86c0c56117
Itsuki5696 <cuijiayi2005@outlook.com>
2025-08-11 11:33:53 +0800

    Add music

--- music/aimyon/おいしいパスタがあると聞いて/logfiles.py
+++ music/aimyon/おいしいパスタがあると聞いて/logfiles.py
@@ -7,7 +7,7 @@ music_list = []
 for filename in os.listdir(dir_path):
     if (filename.endswith(".mp3")):
         files = filename.split('.')
-        id_str = "{id: " + str(int(files[0])) + ", name: '" + files[1] + "', author: 'あいみょん', album: '季億の箱', keywords: ['aimyon', '爱谬'], type: 'music', cover: 'https://github-resources.itsuki.de/music/aimyon/おいしいパスタがあると聞いて/cover.jpg', file: '.https://github-resources.itsuki.de/music/aimyon/おいしいパスタがあると聞いて/" + filename + "', year: 2020, description: ''}"
+        id_str = "{id: " + str(int(files[0])) + ", name: '" + files[1] + "', author: 'あいみょん', album: 'おいしいパスタがあると聞いて', keywords: ['aimyon', '爱谬'], type: 'music', cover: 'https://github-resources.itsuki.de/music/aimyon/おいしいパスタがあると聞いて/cover.jpg', file: 'https://github-resources.itsuki.de/music/aimyon/おいしいパスタがあると聞いて/" + filename + "', year: 2020, description: ''}"
         music_list.append(id_str)
 
 print(','.join(music_list))
